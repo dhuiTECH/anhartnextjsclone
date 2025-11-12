bc61bd7b035a999435fa9db7a1568a92d347dc96
dhuiTECH <damonhui@hotmail.com>
2025-11-12 07:28:16 +0000

    s

--- next-app/src/app/lowlight-config.ts
+++ next-app/src/app/lowlight-config.ts
@@ -1,8 +1,10 @@
-import lowlight from 'lowlight';
-import html from 'highlight.js/lib/languages/xml'; // HTML is often registered as XML
+import { createLowlight } from 'lowlight'; // 1. Import createLowlight
+import html from 'highlight.js/lib/languages/xml';
 import css from 'highlight.js/lib/languages/css';
 import javascript from 'highlight.js/lib/languages/javascript';
 import typescript from 'highlight.js/lib/languages/typescript';
+
+const lowlight = createLowlight(); // 2. Call the function to create the object
 
 // Register the languages you want to support
 lowlight.register('html', html);
