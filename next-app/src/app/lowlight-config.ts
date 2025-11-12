3c2a735b6a07705f215fbf0f7ecc0f96a0b4f083
dhuiTECH <damonhui@hotmail.com>
2025-11-12 07:23:55 +0000

    s

--- next-app/src/app/lowlight-config.ts
+++ next-app/src/app/lowlight-config.ts
@@ -1,9 +1,10 @@
 import { lowlight } from 'lowlight';
-import html from 'highlight.js/lib/languages/xml';
+import html from 'highlight.js/lib/languages/xml'; // HTML is often registered as XML
 import css from 'highlight.js/lib/languages/css';
 import javascript from 'highlight.js/lib/languages/javascript';
 import typescript from 'highlight.js/lib/languages/typescript';
 
+// Register the languages you want to support
 lowlight.register('html', html);
 lowlight.register('css', css);
 lowlight.register('javascript', javascript);
