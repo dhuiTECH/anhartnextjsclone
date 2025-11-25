7fd78996fe0483b15f96fe4d9c9315f1d982bc34
dhuiTECH <damonhui@hotmail.com>
2025-11-25 19:58:49 +0000

    Reduce the amount of CSS overall reducing CSS file loading size

--- next-app/postcss.config.cjs
+++ next-app/postcss.config.cjs
@@ -2,5 +2,18 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? {
+      cssnano: {
+        preset: ['default', {
+          discardComments: { removeAll: true },
+          normalizeWhitespace: true,
+          minifySelectors: true,
+          minifyFontValues: true,
+          minifyParams: true,
+          reduceIdents: false,
+          zindex: false,
+        }],
+      },
+    } : {}),
   },
 };
