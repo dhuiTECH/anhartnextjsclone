f3ee45cf26247d422883f87f697221bc5546b953
dhuiTECH <damonhui@hotmail.com>
2025-11-27 23:03:30 +0000

    removed robots.txt//static file conflicting with dynamic robots

--- next-app/src/app/robots.ts
+++ next-app/src/app/robots.ts
@@ -2,11 +2,89 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/admin', '/member/dashboard', '/api'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/admin/',
+          '/api/',
+          '/member/dashboard/',
+          '/_next/',
+          '/static/',
+          '/private/',
+          '/internal/',
+          '/node_modules/',
+          '/src/',
+          '/dist/',
+          '/*.log$',
+        ],
+      },
+      // Allow Googlebot full access for better indexing
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: ['/admin/', '/api/', '/member/dashboard/', '/_next/'],
+      },
+      {
+        userAgent: 'Googlebot-Image',
+        allow: '/',
+        disallow: ['/admin/', '/api/', '/member/dashboard/'],
+      },
+      {
+        userAgent: 'Googlebot-News',
+        allow: '/',
+        disallow: ['/admin/', '/api/', '/member/dashboard/'],
+      },
+      // Allow Bingbot full access
+      {
+        userAgent: 'Bingbot',
+        allow: '/',
+        disallow: ['/admin/', '/api/', '/member/dashboard/', '/_next/'],
+      },
+      // Allow social media bots
+      {
+        userAgent: 'Twitterbot',
+        allow: '/',
+        disallow: ['/admin/', '/api/', '/member/dashboard/'],
+      },
+      {
+        userAgent: 'facebookexternalhit',
+        allow: '/',
+        disallow: ['/admin/', '/api/', '/member/dashboard/'],
+      },
+      {
+        userAgent: 'LinkedInBot',
+        allow: '/',
+        disallow: ['/admin/', '/api/', '/member/dashboard/'],
+      },
+      {
+        userAgent: 'WhatsApp',
+        allow: '/',
+        disallow: ['/admin/', '/api/', '/member/dashboard/'],
+      },
+      // Block problematic/scraper bots
+      {
+        userAgent: 'AhrefsBot',
+        disallow: ['/'],
+      },
+      {
+        userAgent: 'MJ12bot',
+        disallow: ['/'],
+      },
+      {
+        userAgent: 'DotBot',
+        disallow: ['/'],
+      },
+      {
+        userAgent: 'SemrushBot',
+        disallow: ['/'],
+      },
+      {
+        userAgent: 'BLEXBot',
+        disallow: ['/'],
+      },
+    ],
     sitemap: 'https://anhart.ca/sitemap.xml',
     host: 'https://anhart.ca',
   };
